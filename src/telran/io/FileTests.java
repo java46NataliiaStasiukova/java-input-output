17ad13911f72629f1f5a2d20ca533b9375d46445
Nataliia <nstasiukova@gmail.com>
2022-10-12 21:07:28 +0300

    Homework-32

--- src/telran/io/FileTests.java
+++ src/telran/io/FileTests.java
@@ -1,15 +1,15 @@
 package telran.io;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.*;
 import java.io.*;
 class FileTests {
 	File file;
 	@BeforeEach
 	void setUp() {
-		file = new File("dir1/dir2");
+		file = new File("dir1");
 		file.delete();
+
 	}
 
 	@Test
@@ -20,8 +20,9 @@ class FileTests {
 		assertTrue(file.exists());
 	}
 	@Test
-	void printDirectoryContent() {
-		printDirectory("..", 2);
+	void printDirectoryContent() throws IOException {
+		createFiles();
+		printDirectory("testFolder", -1, 1);
 	}
 	/**
 	 * 
@@ -32,8 +33,23 @@ class FileTests {
 	 *          ''''''''
 	 *          level = -1 printing all levels
 	 */
-	private void printDirectory(String pathName, int level) {
-		// TODO 
+	private void printDirectory(String pathName, int level, int indent) {
+		if(level == 0) {
+			return;
+		}
+		File filePath = new File(pathName);
+		File files[] = filePath.listFiles(); 
+		String fileOrFolder = "Directory";
+		for(File f: files) {
+			if(f.isFile()) {
+				fileOrFolder = "File";
+		    }
+			System.out.printf("%s%s%s%s%s\n", " ".repeat(indent), "<", f.getName(), "> type = ", fileOrFolder);	
+			if(f.isDirectory()) {		
+				printDirectory(f.getPath(), level - 1, indent + 4);		
+			}
+		}
+		
 		/*
 		   <dir> type=dir
 		       <dir> type=dir
@@ -43,5 +59,29 @@ class FileTests {
 		 */
 		
 	}
+	private void createFiles() throws IOException {	
+		File[] arrayDirs = {
+				new File("testFolder"),
+				new File("testFolder/f1"),
+				new File("testFolder/f2/f5/f6"),
+				new File("testFolder/f3/f4")
+		};
+		File[] arrayFiles = {	
+				new File("testFolder/f1", "t1.txt"),
+				new File("testFolder/f2", "t2.txt"),
+				new File("testFolder/f2/f5", "t3.txt"),
+				new File("testFolder/f2/f5", "t4.txt"),
+				new File("testFolder/f2/f5/f6", "t5.txt")		
+		};
+		
+		for(File file: arrayDirs) {
+			file.mkdirs();
+		}
+		for(File file: arrayFiles) {
+			file.createNewFile();
+		}
+
+	}
+
 
 }
