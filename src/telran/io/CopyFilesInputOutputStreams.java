d3edd094da87aa96c2c87dc74c7f1fceb56550e6
Nataliia <nstasiukova@gmail.com>
2022-10-19 13:11:51 +0300

    Homework-32

--- src/telran/io/CopyFilesInputOutputStreams.java
+++ src/telran/io/CopyFilesInputOutputStreams.java
@@ -1,22 +1,81 @@
 package telran.io;
 
-/**
- * Application for copying files based on FileInputStream and FileOutputStream
- * files may be very big (several Gbytes ) 
- * args[0] - source file path
- * args[1] - destination file path
- * args[2] - if exists "overwritten" then destination may be overwritten otherwise may not be
- * Output one of the following:
- * Files have been copied (<amount of bytes> <time of copying>)
- * Source file doesn't exist
- * Destination can not be overwritten
- *
- */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class CopyFilesInputOutputStreams {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] count = {0};
+		LocalDateTime start = LocalDateTime.now();
+		boolean overwritten = false;
+		if (args.length > 2) {
+			overwritten = true;
+		}
+		try {
+			if (args.length < 2) {
+				throw new Exception("Not enough arguments for copying");
+			}
+			File source = getSource(args[0]);
+			File destination = getDestination(args[1], overwritten);
+			copyFiles(source, destination, count);
+			LocalDateTime end = LocalDateTime.now();
+			System.out.println("Copied <" + count[0] + " bytes> for <running time in milliseconds: " + ChronoUnit.MILLIS.between(start, end) + ">");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
 
+	@SuppressWarnings("resource")
+	private static void copyFiles(File source, File target, int[] count) throws Exception {
+		if (source.isDirectory()) {
+			if (!target.exists()) {
+				target.mkdir();
+			}
+			String[] children = source.list();
+			for (int i = 0; i < children.length; i++) {
+				copyFiles(new File(source, children[i]), new File(target, children[i]), count);
+			}
+		} else {
+			InputStream input = new FileInputStream(source);
+			OutputStream output;
+			try {
+				output = new FileOutputStream(target);
+			} catch (FileNotFoundException e) {
+				throw new Exception("Destination <" + target + "> has non-existed directory in the path.");
+			}
+			byte[] buffer = new byte[1024];
+			int len;
+			while ((len = input.read(buffer)) > 0) {
+				count[0] += len;
+				output.write(buffer, 0, len);
+			}
+			input.close();
+			output.close();
+		}
+	}
+
+	private static File getDestination(String path, boolean overwritten) throws Exception {
+		File file = new File(path);
+		if (file.exists() && overwritten == false) {
+			throw new Exception("Destination <" + path + "> cannot be overwritten");
+		}
+		return file;
+	}
+
+	private static File getSource(String path) throws Exception {
+		File sourceFile = new File(path);
+		if (!sourceFile.exists()) {
+			throw new Exception("Source file <" + path + "> does not exist");
+		}
+		return sourceFile;
 	}
 
 }
--- src/telran/io/CopyFilesStandard.java
+++ src/telran/io/CopyFilesStandard.java
@@ -1,22 +1,72 @@
 package telran.io;
 
-/**
- * Application for copying files based on static method copy of class Files
- * files may be very big (several Gbytes ) 
- * args[0] - source file path
- * args[1] - destination file path
- * args[2] - if exists "overwritten" then destination may be overwritten otherwise may not be
- * Output one of the following:
- * Files have been copied (<amount of bytes> <time of copying>)
- * Source file doesn't exist
- * Destination can not be overwritten
- *
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class CopyFilesStandard {
+		
+		public static void main(String[] args) {
+			int[] count = {0};
+			LocalDateTime start = LocalDateTime.now();
+			boolean overwritten = false;
+			if (args.length > 2) {
+				overwritten = true;
+			}
+			try {
+				if (args.length < 2) {
+					throw new Exception("Not enough arguments for copying");
+				}
+				File source = getSource(args[0]);
+				File destination = getDestination(args[1], overwritten);
+				copyFiles(source, destination, count);
+				LocalDateTime end = LocalDateTime.now();
+				System.out.println("Copied <" + count[0] + " bytes> for <running time in milliseconds: " + ChronoUnit.MILLIS.between(start, end) + ">");
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			
+		}
+		private static void copyFiles(File source, File target, int[] count) throws Exception {
+			if (source.isDirectory()) {
+				if (!target.exists()) {
+					target.mkdir();
+				}
+				String[] children = source.list();
+				for (int i = 0; i < children.length; i++) {
+					copyFiles(new File(source, children[i]), new File(target, children[i]), count);
+				}
+			} else {
+				OutputStream output;
+				try {
+					output = new FileOutputStream(target);
+				} catch (FileNotFoundException e) {
+					throw new Exception("Destination <" + target + "> has non-existed directory in the path.");
+				}
+				count[0] += Files.copy(Path.of(source.getPath()), output);
+				output.close();
+			}
+		}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		private static File getDestination(String path, boolean overwritten) throws Exception {
+			File file = new File(path);
+			if (file.exists() && overwritten == false) {
+				throw new Exception("Destination <" + path + "> cannot be overwritten");
+			}
+			return file;
+		}
 
-	}
+		private static File getSource(String path) throws Exception {
+			File sourceFile = new File(path);
+			if (!sourceFile.exists()) {
+				throw new Exception("Source file <" + path + "> does not exist");
+			}
+			return sourceFile;
+		}
 
 }
